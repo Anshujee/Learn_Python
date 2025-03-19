a5861f965abf10ea5aac7aefe56cd0f26a04739c
Anshu <jeeanshu24@gmail.com>
2025-03-19 12:20:50 +0530

    Python Example

---
+++ Day4/calculate_age.py
@@ -0,0 +1,5 @@
+name = input ("what is your Name ?")
+birth_year = int(input(" What is your Year of Birth ?"))
+current_year = 2025
+age = current_year - birth_year
+print ( "Hello ! " + name + "you are "+ str(age) + " years old.")
---
+++ Day4/hello.py
@@ -0,0 +1,9 @@
+print("Hello!Anshu Welcome to Python!")
+
+#using Input function
+name = input ("Please Enter your name: "  )
+age = input  (" Please Enter your Age: ")
+city = input ( "Enter the city where you live: ")
+print (" Hello !"+ name + " Welcome to the World of Python I know you are " + age + " Year old and Lived in " + city + "")
+
+hh
