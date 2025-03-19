fa182bfac06c0680261c82cce644a8d7cdac9cdb
Anshu <jeeanshu24@gmail.com>
2025-03-19 12:24:12 +0530

    Add Comment

--- Day4/calculate_age.py
+++ Day4/calculate_age.py
@@ -1,5 +1,10 @@
+# Ask for the user's name
 name = input ("what is your Name ?")
+# Ask for the birth year and convert it to an integer
 birth_year = int(input(" What is your Year of Birth ?"))
+# Calculate the age (assuming the current year is 2025)
 current_year = 2025
+
 age = current_year - birth_year
+# Print a personalized message with age
 print ( "Hello ! " + name + "you are "+ str(age) + " years old.")
