3fc7d4db6363628421e23e1ae9fc9b19d8fc2b6a
Anshu <jeeanshu24@gmail.com>
2025-05-17 20:46:29 +0530

    New File

---
+++ Day5/var.py
@@ -0,0 +1,20 @@
+# Python Variables
+# In Python, variables are used to store data that can be referenced 
+# and manipulated during program execution. A variable is essentially 
+# a name that is assigned to a value. 
+# Variables act as placeholders for data. 
+# They allow us to store and reuse values in our program.
+#Example:
+
+
+# Variable 'x' stores the integer value 10
+
+x = 5
+
+# Variable 'name' stores the string "Samantha"
+
+name = "Samantha"  
+
+print(x)
+
+print(name)
