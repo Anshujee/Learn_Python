94361de6c4222b018e1366b16867f4e867240613
Anshu <jeeanshu24@gmail.com>
2025-05-20 11:39:41 +0530

    2

--- Day6/opr.py
+++ Day6/opr.py
@@ -47,4 +47,17 @@ print("Exponentiation:", a**b)
 
 ########################################################################
 
+# Comparison of Python Operators or Relational Operator 
+# In Python Comparison of Relational operators compares the values. 
+# It either returns True or False according to the condition.
+# Example 2 --- 
+a = 13
+b = 33
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
 
