1bc65b880630ac4256561b00820b7f9298990090
Anshu <jeeanshu24@gmail.com>
2025-05-20 11:11:27 +0530

    Day6 Opertor

---
+++ Day6/opr.py
@@ -0,0 +1,23 @@
+#########################################################################
+                    #    Python Operators
+# In Python, operators are special symbols or keywords used to perform operations
+# on variables and values. 
+# Operators are essential for writing logical, mathematical, and functional code.
+# Operators in general are used to perform operations on values and variables. 
+##############################################################################
+# Types of Operators in Python
+#  1 . Arithmetic Operators
+#  2.  Comparison Operators
+#  3.  Logical Operators
+#  4.  Bitwise Operators
+#  5.  Assignment Operators
+#  6.  Identity Operators and Membership Operators
+#############################################################################
+# Arithmetic Operators in Python
+# Python Arithmetic operators are used to perform basic mathematical 
+# operations like addition, subtraction, multiplication and division.
+# Example 1 ---
+#variable 
+a = 5 
+b = 10
+print ("Addition : ", a+b)
