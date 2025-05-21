d1313bece2abd5a3784ed8cba6bc280c9ec4ec65
Anshu <jeeanshu24@gmail.com>
2025-05-21 12:35:54 +0530

    Data Type 2

--- Day8/Datatype.py
+++ Day8/Datatype.py
@@ -97,4 +97,127 @@ print (nested)
 
 # Ideal for fixed data sets like days of the week, coordinates, config settings
 
-########################################################################
+################################################################################
+
+###################### Set Data Type in Python ##############################
+# In Python Data Types, Set is an unordered collection of data types that is 
+# iterable, mutable, and has no duplicate elements. The order of elements in 
+# a set is undefined though it may consist of various elements.
+my_set = {1, 2, 3}
+print (my_set)
+print(type(my_set))  # Output: <class 'set'>
+
+# Why Use Sets?
+# Automatically remove duplicates
+
+# Perform mathematical set operations (union, intersection, difference)
+
+# Efficient membership testing (very fast in operation)
+
+# Clean up data (e.g., remove duplicate IPs or config items)
+
+numbers1= {1, 2, 3, 4}
+mixed1= {1, "Anshu", True, 3.14}
+duplicates = {1, 2, 2, 3, 3, 3}
+print (numbers1)
+print (mixed1)
+print(duplicates)  # Output: {1, 2, 3}
+##############################################################################
+#   Set Operations
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print(a | b)   # Union: {1, 2, 3, 4, 5}
+print(a & b)   # Intersection: {3}
+print(a - b)   # Difference: {1, 2}
+print(a ^ b)   # Symmetric Difference: {1, 2, 4, 5}
+
+##################################################################
+
+# Real-World DevOps Example
+# Remove duplicate AWS regions in a config:
+aws_regions = ["us-east-1", "us-west-1", "us-east-1", "eu-central-1"]
+unique_regions = set(aws_regions)
+
+print(f"Deploying to regions: {unique_regions}")
+
+#####################################################################
+# Compare IPs from two sources:
+
+
+dev_ips = {"10.0.0.1", "10.0.0.2"}
+prod_ips = {"10.0.0.2", "10.0.0.3"}
+
+common_ips = dev_ips & prod_ips
+print(f"IPs in both Dev and Prod: {common_ips}")
+
+#######################################################################
+##################### Dictionary Data Type ##########################
+# A Dictionary is a collection of key-value pairs
+# Dictionary holds a key: value pair. Key-value is provided in the dictionary 
+# to make it more optimized. Each key-value pair in a Dictionary is separated by 
+# a colon : , whereas each key is separated by a ‘comma’.
+
+my_disc = { "name" : "Anshu" ,
+           "Role" : "DevOps Engineer ",
+             "Location" : "Pune", 
+               "Company" : "Infosys"  }
+print (my_disc)
+print (my_disc["name"])
+print (my_disc["Role"])
+print (my_disc["Location"])
+# Another Way to print 
+
+print ("Seprate")
+print (my_disc.get("name"))
+print (my_disc.get("Role"))
+print (my_disc.get("Location"))
+print (my_disc.get("Company"))
+
+#############################################################################
+    ############## Real-World DevOps Examples ################
+   
+    # 1.  Store EC2 instance metadata
+
+ec2_instance = {
+    "id": "i-1234567890",
+    "type": "t2.micro",
+    "region": "us-east-1",
+    "status": "running"
+}
+
+print (ec2_instance .get ("type"))
+print (ec2_instance.get ("region"))
+
+# 2. Track Docker container states
+docker_status = {
+    "nginx": "running",
+    "redis": "stopped",
+    "postgres": "running"
+}
+
+print (docker_status.get("nginx"))
+print (docker_status.get ("redis"))
+
+# 3. Map environment to config
+
+env_config = {
+    "dev": {"instance_type": "t2.micro", "count": 2},
+    "prod": {"instance_type": "m5.large", "count": 5}
+}
+
+print (env_config)
+
+# Example: Dictionary + Conditional Logic 
+aws_resources = {
+    "ec2": "Running",
+    "rds": "stopped",
+    "s3": "active"
+}
+
+if aws_resources.get("ec2") == "running":
+    print("EC2 is up and running.")
+else:
+    print ("EC2 is not running .Please restart the Instance")
+
+
