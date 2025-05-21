41af3c831e71cf53d27f1f59ae97df622cc86b19
Anshu <jeeanshu24@gmail.com>
2025-05-21 10:39:30 +0530

    Keyword Concept in Python

---
+++ Day7/key.py
@@ -0,0 +1,16 @@
+           ##########  Python Keywords #######
+# Keywords in Python are reserved words that have special meanings and 
+# serve specific purposes in the language syntax. Python keywords cannot 
+# be used as the names of variables, functions, and classes or any other 
+# identifier.
+# Getting List of all Python keywords
+import keyword
+# printing all keywords at once using "kwlist()"
+print ("The list of Keyword is : " )
+print (keyword.kwlist)
+# OUTPUT is the list of Keyword 
+The list of Keyword is : 
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 
+# 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 
+# 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+#  'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
