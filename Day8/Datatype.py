022499913a8f5fdd074aedc9b1be88a8521cf779
Anshu <jeeanshu24@gmail.com>
2025-05-21 11:56:11 +0530

    Data Types 1

--- Day8/Datatype.py
+++ Day8/Datatype.py
@@ -21,6 +21,7 @@
 # Python List
 # Python Tuple
 
+########################################################################
 # String Data Type
 # Python Strings are arrays of bytes representing Unicode characters. In Python, 
 # there is no character data type Python, a character is a string of length one. 
@@ -41,3 +42,59 @@ print(s[1])
 print(s[2])
 print(s[-1])
 print(s[-2])
+
+
+###############################################################################
+# List Data Type
+# A list is an ordered, mutable (changeable) collection that allows duplicate elements.
+# It is one of the most commonly used data types in Python.
+# Lists are just like arrays, declared in other languages which is an ordered 
+# collection of data. 
+# It is very flexible as the items in a list do not need to be of the same type.
+# Creating a List in Python
+
+# Lists in Python can be created by just placing the sequence inside the square brackets[].
+# Empty list
+a = []
+
+# list with int values
+a = [1, 2, 3]
+print(a)
+
+# list with mixed int and string
+b = ["Anshu", "Pune", "Infosys", 4, 5]
+print(b)
+
+
+##########################################################################
+# Tuple Data Type
+# Just like a list, a tuple is also an ordered collection of Python objects. 
+# The only difference between a tuple and a list is that tuples are immutable. 
+# Tuples cannot be modified after it is created.
+# Creating a Tuple in Python
+
+# In Python Data Types, tuples are created by placing a sequence of values 
+# separated by a ‘comma’ with or without the use of parentheses for grouping 
+# the data sequence. Tuples can contain any number of elements and of any datatype
+#  (like strings, integers, lists, etc.).
+
+empty_tuple = ()
+numbers = (1, 2, 3, 4)
+mixed = (1, "Anshu", True, 3.14)
+nested = (1, [2, 3], (4, 5))
+
+print (empty_tuple)
+print (numbers)
+print(mixed)
+print (nested)
+
+
+#######################################################################
+# Why Use Tuples?
+# To protect data from being modified
+
+# Tuples use less memory and are faster than lists
+
+# Ideal for fixed data sets like days of the week, coordinates, config settings
+
+########################################################################
