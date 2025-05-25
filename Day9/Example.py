bfa1923cf856fa502cd032723c53ad314bdd74f6
Anshu <jeeanshu24@gmail.com>
2025-05-25 19:08:40 +0530

    Ex -8

--- Day9/Example.py
+++ Day9/Example.py
@@ -105,4 +105,26 @@ elif a%5 == 0:
 else:
     print (a)
 
+ ######################################################################
+ # Example 7 
+ # Question --- 
+ # Given a number n, number of apples in a bag. You and your friend are 
+ # picking one apple turnwise from the bag. It is given that the first attempt 
+ # is always by you. The person picking the last apple will be the winner. 
 
+# If you will win: print "You" (without quotes)
+# If your friend will win: print "Friend" (without quotes)   
+
+n = int(input("number of apple : " ))
+if n % 2 == 1 :
+    print ("You are the winner - Anshu")
+else:
+    print ("Your Friend is winner - Anshu ")
+
+###################################################################
+
+m = int(input(" Please choose a number :  "))
+if m % 2 == 1:
+    print ("odd")
+else:
+    print("Even")
