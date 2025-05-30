f6a3bf5219cc6df771f2902c6f7189790dff5691
Anshu <jeeanshu24@gmail.com>
2025-05-30 11:35:58 +0530

    More Loop Example

---
+++ Day11/exa.py
@@ -0,0 +1,12 @@
+from __future__ import print_function
+for i in range(1, 5):
+    for j in range(i):
+        print(i, end=' ')
+    print()
+
+
+fruits = ["apple", "orange", "kiwi"]
+
+for fruit in fruits:
+
+    print(fruit)
