8dcf8b7942518ea7152c86b01a11cf50aac56d8a
Anshu <jeeanshu24@gmail.com>
2025-10-08 11:18:41 +0530

    Concept Env_var

--- Day17/env_var.py
+++ Day17/env_var.py
@@ -3,3 +3,19 @@ import os
 # Get the value of the environment variable 'password'
 print (os.getenv('password'))
 print (os.getenv('user')) # Another way to get the value of the environment variable 'user'
+
+# To Set the envvariable in terminal use the below command
+# export password='your_password'
+# export user='your_user'
+# To run the program use the below command
+# python3 env_var.py by importing os module
+# To unset the envvariable in terminal use the below command
+# unset password
+# unset user
+# To check the envvariable in terminal use the below command
+# printenv | grep password
+# printenv | grep user
+# echo $password
+# echo $user
+# Note: The environment variable will be available only in the current terminal session.
+#  If you open a new terminal, you will need to set the variable again.
