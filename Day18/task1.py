1568435b544de2b1cc97f3c61a80bf032efa408e
Anshu <jeeanshu24@gmail.com>
2025-10-09 12:06:32 +0530

    Day18

---
+++ Day18/task1.py
@@ -0,0 +1,25 @@
+# Create two variables a and b with numeric values.
+# Calculate the sum, difference, product, and quotient of a and b.
+# Print the results.
+a = 10
+b = 5
+sum_result = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+float_division = a // b
+reminder = a % b
+
+
+print("Sum:", sum_result)
+
+print("Difference:", difference)
+
+print("Product:", product)
+
+print("Quotient:", quotient)
+
+print("Float Division:", float_division)
+
+print("Reminder:", reminder)
+
---
+++ Day18/task2.py
@@ -0,0 +1,19 @@
+# Compare the values of a and b using the following comparison operators: <, >, <=, >=, ==, and !=.
+# Print the results of each comparison.
+
+a = 10
+b = 5
+
+less_than = a < b
+greater_than = a > b
+less_than_equal = a <= b
+greater_than_equal = a >= b
+equal = a == b
+not_equal = a != b
+print("a < b:", less_than)
+print("a > b:", greater_than)
+print("a <= b:", less_than_equal)
+print("a >= b:", greater_than_equal)
+print("a == b:", equal)
+print("a != b:", not_equal)
+# Use logical operators (and, or, not) to combine some of the comparison results.
