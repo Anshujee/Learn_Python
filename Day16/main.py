6c2e05a2580c5b2f5d1c20af8e40f250ee64782d
Anshu <jeeanshu24@gmail.com>
2025-10-28 11:21:52 +0530

    main commit

--- Day16/main.py
+++ Day16/main.py
@@ -1,8 +1,8 @@
-import devops_utils as du
+import devops_utils
 
 def main():
-    du.greet_engineer("Anshu")
-    du.check_server_status("Production_Server_1")
-if __name__ == "__main__":
-    main()
+    devops_utils.greet_engineer("Anshu")
+    devops_utils.check_server_status("Production_Server_1")
+    
+main()
 
